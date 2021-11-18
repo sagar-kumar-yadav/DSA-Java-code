1101d787a80d23f35c76696e02932f2bd69ccf71
sagar kumar yadav <sk5323912@gmail.com>
2021-11-18 22:57:58 +0530

    new files

---
+++ 09-Searching-Assignments/src/com/sagar/Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,36 @@
+package com.sagar;
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// 744. Find The Smallest Letter Greater Than Target
+
+public class Find_Smallest_Letter_Greater_Than_Target {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'f';
+        System.out.print(nextGreatestLetter(letters, target));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+
+       /*
+        // brute force
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] > target){
+                return letters[i];
+            }
+        }
+        return letters[0];
+        */
+
+        // binary search
+        int start = 0;
+        int end = letters.length-1;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if (letters[mid]==target){
+                
+            }
+        }
+    }
+}
