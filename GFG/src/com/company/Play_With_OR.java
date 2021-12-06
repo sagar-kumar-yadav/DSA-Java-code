bbdb4bd2b045ab7ec321f13878a7ff2de22794da
sagar kumar yadav <sk5323912@gmail.com>
2021-12-06 21:04:14 +0530

    new commit

---
+++ GFG/src/com/company/Play_With_OR.java
@@ -0,0 +1,20 @@
+package com.company;
+
+// https://practice.geeksforgeeks.org/problems/play-with-or5515/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-1page1category[]Arrays
+// Play With OR
+
+import java.util.Arrays;
+
+public class Play_With_OR {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = {10, 11, 1, 2, 3};
+        System.out.println(Arrays.toString(game_with_number(arr, n)));
+    }
+    static int[] game_with_number(int[] arr, int n){
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i] | arr[i+1];
+        }
+        return arr;
+    }
+}
---
+++ GFG/src/com/company/Red_Or_Green.java
@@ -0,0 +1,31 @@
+package com.company;
+
+// https://practice.geeksforgeeks.org/problems/red-or-green5711/1/?category[]=Strings&category[]=Strings&problemStatus=unsolved&difficulty[]=-1&page=1&query=category[]StringsproblemStatusunsolveddifficulty[]-1page1category[]Strings
+// Red OR Green
+
+public class Red_Or_Green {
+    public static void main(String[] args) {
+        int N = 5;
+        String S = "RGRGR";
+        System.out.println(RedOrGreen(N, S));
+    }
+    static int RedOrGreen(int N, String S){
+        int ans = 0;
+        int rCount = 0, gCount = 0;
+        for (int i = 0; i < N; i++) {
+            if (S.charAt(i) == 'R'){
+                rCount++;
+            }
+            else if (S.charAt(i) == 'G'){
+                gCount++;
+            }
+            if (rCount > gCount){
+                ans = gCount;
+            }
+            else {
+                ans = rCount;
+            }
+        }
+        return ans;
+    }
+}
