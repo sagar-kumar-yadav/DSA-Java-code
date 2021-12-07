83247bd4162e3bc5b0d4549c7d51405d0771472e
sagar kumar yadav <sk5323912@gmail.com>
2021-12-08 01:33:36 +0530

    new update

---
+++ GFG/src/com/company/Triplets_With_Sum_With_given_range.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Triplets_With_Sum_With_given_range {
+    public static void main(String[] args) {
+        int N = 5;
+        int[] Arr = {5, 1 ,4, 3, 2};
+        int L = 2;
+        int R = 7;
+        System.out.println(countTriplets(Arr, N, L, R));
+    }
+    static int countTriplets(int Arr[], int N, int L, int R){
+        // brute-force
+       int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int k = j+1; k < N; k++) {
+                    if (Arr[i] + Arr[j] + Arr[k] >= L && Arr[i] + Arr[j] + Arr[k] <= R){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+
+        // two - pointer
+
+    }
+}
