7851d3d8b337ea12b508dc2834016452e0a92981
sagar kumar yadav <sk5323912@gmail.com>
2021-12-08 18:02:34 +0530

    new commit

---
+++ 17-Recursion/src/com/sagar/Find_If_Array_Is_sorted_Or_not.java
@@ -0,0 +1,7 @@
+package com.sagar;
+
+public class Find_If_Array_Is_sorted_Or_not {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 17-Recursion/src/com/sagar/arrays/Find_If_Array_Is_Sorted_Or_Not.java
@@ -0,0 +1,4 @@
+package com.sagar.arrays;
+
+public class Find_If_Array_Is_Sorted_Or_Not {
+}
