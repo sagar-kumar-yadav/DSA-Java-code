1b75d47873031ff357a6d72c780727e61853dfa5
sagar-kumar-yadav <sk5323912@gmail.com>
2023-04-15 21:33:44 +0530

    new commit

--- 02-BasicsOfArrays/src/com/assignment/Move_All_Zeros_To_The_End.java
+++ 02-BasicsOfArrays/src/com/assignment/Move_All_Zeros_To_The_End.java
@@ -1,2 +1,76 @@
-package com.assignment;public class Move_All_Zeros_To_The_End {
+package com.assignment;
+
+public class Move_All_Zeros_To_The_End {
+    public static void main(String[] args) {
+//        int[] arr = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+        int[] arr = {0,1,0,3,12};
+//        pushZerosToEnd(arr, 5);
+//        pushZerosToEnd1(arr, 5);
+        pushZerosToEnd2(arr, 5);
+
+    }
+    static void pushZerosToEnd(int[] arr, int n) {
+        // code here
+        int temp[] = new int[n];
+        int k=0;
+        for (int i=0;i<n;i++){
+            if (arr[i]!=0){
+                temp[k]=arr[i];
+                k++;
+            }
+        }
+
+        while (k<n){
+            temp[k]=0;
+            k++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(temp[i]+" ");
+        }
+    }
+
+    static void pushZerosToEnd1(int[] arr, int n) {
+        int k = 0;
+        while (k < n) {
+            if (arr[k] == 0) {
+                break;
+            }else {
+                k = k +1;
+            }
+        }
+
+        int i = k, j = k + 1;
+
+        while (i < arr.length && j < arr.length) {
+            if (arr[j] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+
+            }
+
+            j++;
+
+        }
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void pushZerosToEnd2(int[] arr, int n) {
+        int j = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                j++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
--- 02-BasicsOfArrays/src/com/assignment/Union_Of_Two_Sorted_Arrays.java
+++ 02-BasicsOfArrays/src/com/assignment/Union_Of_Two_Sorted_Arrays.java
@@ -1,2 +1,30 @@
-package com.assignment;public class Union_Of_Two_Sorted_Arrays {
+package com.assignment;
+
+import java.util.*;
+
+public class Union_Of_Two_Sorted_Arrays {
+    public static void main(String[] args) {
+        int n = 2;
+        int m = 8;
+        int[] arr1 = {1, 35};
+        int[] arr2 = {6, 9, 13, 15, 20, 25, 29, 46};
+        System.out.println(findUnion(arr1, arr2, n, m));
+    }
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m) {
+        HashSet<Integer> s = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            s.add(arr1[i]);
+        }
+        for (int i = 0; i < m; i++) {
+            s.add(arr2[i]);
+        }
+        Iterator<Integer> iterator = s.iterator();
+        while (iterator.hasNext()) {
+            int it = iterator.next();
+            list.add(it);
+        }
+        Collections.sort(list);
+        return list;
+    }
 }
